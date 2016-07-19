a2ccb3cd193ceeed2ce2176a34c69f963f1b2388
Ben Briggs <ben.s.briggs@gmail.com>
2016-07-19 16:48:07 -0400

    redo build to output UMD and commonjs

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Editor from './components/Editor';
+import createPlugin from './plugins/createPlugin';
+import pluginUtils from './plugins/utils';
+import compose from './util/compose';
+
+export {
+  Editor,
+  createPlugin,
+  pluginUtils,
+  compose
+};
